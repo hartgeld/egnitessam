89f027cb455deb27cfdf7a0ccaf77e5a4454ae97
hartgeld <lorehartgeld@gmail.com>
2016-01-27 01:52:04 +0100

    carousel fullscreen

--- myjs/setItemToActive.js
+++ myjs/setItemToActive.js
@@ -1,8 +1,11 @@
     
-	// carousel: set first item(class) to active
 	window.onload = function() {
+		// carousel: set first item(class) to active
 	    var x = document.getElementsByClassName("item").length;
 	    console.log(x);
 	    var allItems = document.getElementsByClassName("item");
 	    allItems[0].className += ' active'
+	    //carousel_pager set first pagerItem(class) to active 
+	    var allPagerItems = document.getElementsByClassName("pager-item");
+	    allPagerItems[0].className += ' active'
 	}
